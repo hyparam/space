256efba54119372a12a6849cfa2e93adba0f6ed2
Sylvain Lesage <severo@rednegra.net>
2024-10-15 14:32:07 +0200

    fix css by renaming id

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-const root = document.getElementById('root')
+const root = document.getElementById('app')
 if (!root) throw new Error('No root element')
 createRoot(root).render(
   <StrictMode>
