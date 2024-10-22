fcf60b2ff94a6bdc502706e6640813abe5a8517c
Sylvain Lesage <severo@rednegra.net>
2024-10-22 22:20:39 +0200

    refactor

--- src/Login.tsx
+++ src/Login.tsx
@@ -21,27 +21,28 @@ export async function fetchOAuth(): Promise<OAuthResult | undefined> {
     localStorage.setItem("oauth", JSON.stringify(oauthResult));
     return oauthResult;
   } else {
-    const options = (
-      !("huggingface" in window) ||
-      !window.huggingface ||
-      typeof window.huggingface !== "object" ||
-      !("variables" in window.huggingface) ||
-      !window.huggingface.variables ||
-      typeof window.huggingface.variables !== "object" ||
-      !("OAUTH_SCOPES" in window.huggingface.variables) ||
-      !window.huggingface.variables.OAUTH_SCOPES ||
-      typeof window.huggingface.variables.OAUTH_SCOPES !== "string" ||
-      !("OAUTH_CLIENT_ID" in window.huggingface.variables) ||
-      !window.huggingface.variables.OAUTH_CLIENT_ID ||
-      typeof window.huggingface.variables.OAUTH_CLIENT_ID !== "string"
-    ) ? {
-      clientId: "921c40c6-531f-419e-9aa8-3d1cc2606e5e",// obtained by creating an app at https://huggingface.co/settings/applications
-      // TODO(SL): don't hardcode the clientId
-      scopes: "openid profile read-repos",
-    } : {
-      clientId: window.huggingface.variables.OAUTH_CLIENT_ID,
-      scopes: window.huggingface.variables.OAUTH_SCOPES,
-    }
+    const options =
+      "huggingface" in window &&
+      window.huggingface &&
+      typeof window.huggingface === "object" &&
+      "variables" in window.huggingface &&
+      window.huggingface.variables &&
+      typeof window.huggingface.variables === "object" &&
+      "OAUTH_SCOPES" in window.huggingface.variables &&
+      window.huggingface.variables.OAUTH_SCOPES &&
+      typeof window.huggingface.variables.OAUTH_SCOPES === "string" &&
+      "OAUTH_CLIENT_ID" in window.huggingface.variables &&
+      window.huggingface.variables.OAUTH_CLIENT_ID &&
+      typeof window.huggingface.variables.OAUTH_CLIENT_ID === "string"
+        ? {
+            clientId: window.huggingface.variables.OAUTH_CLIENT_ID,
+            scopes: window.huggingface.variables.OAUTH_SCOPES,
+          }
+        : {
+            clientId: "921c40c6-531f-419e-9aa8-3d1cc2606e5e", // obtained by creating an app at https://huggingface.co/settings/applications
+            // TODO(SL): don't hardcode the clientId
+            scopes: "openid profile read-repos",
+          };
 
     // prompt=consent to re-trigger the consent screen instead of silently redirecting
     window.location.href = (await oauthLoginUrl(options)) + "&prompt=consent";
@@ -51,5 +52,5 @@ export async function fetchOAuth(): Promise<OAuthResult | undefined> {
 export async function fetchOAuthToken(): Promise<string> {
   const oauth = await fetchOAuth();
   if (!oauth) throw new Error("No oauth");
-  return oauth.accessToken
+  return oauth.accessToken;
 }
