f4961d65afed7108fd105a44a4b6314a979c0d03
Sylvain Lesage <severo@rednegra.net>
2024-10-23 11:08:57 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { AuthProvider } from "./contexts/AuthContext.tsx";
 
 export default function App() {
   console.log('url', window.location.href);
+  console.log('search', window.location.search);
   return (
     <AuthProvider>
       <Page />
