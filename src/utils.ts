29af07b281c56070c87b8113744397b13f3e16d9
Sylvain Lesage <severo@rednegra.net>
2024-10-23 23:10:13 +0200

    fix private/gated for parquet / head requests

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,4 @@
-import { byteLengthFromUrl, AsyncBuffer } from "hyparquet";
+import { AsyncBuffer } from "hyparquet";
 
 /**
  * Helper function to join class names
@@ -13,21 +13,42 @@ interface AsyncBufferFromUrlOptions {
   headers?: Record<string, string>;
 }
 
+/**
+ * Get the byte length of a URL using a HEAD request.
+ *
+ * @param {string} url
+ * @returns {Promise<number>}
+ */
+export async function byteLengthFromUrl(
+  url: RequestInfo | URL,
+  init?: RequestInit
+): Promise<number> {
+  return await fetch(url, { ...init, method: "HEAD" }).then((res) => {
+    if (!res.ok) throw new Error(`fetch head failed ${res.status.toString()}`);
+    const length = res.headers.get("Content-Length");
+    if (!length) throw new Error("missing content length");
+    return parseInt(length);
+  });
+}
+
 export async function asyncBufferFromUrl({
   url,
   byteLength,
-  headers: customHeaders,
+  headers,
 }: AsyncBufferFromUrlOptions): Promise<AsyncBuffer> {
   // byte length from HEAD request
-  byteLength ||= await byteLengthFromUrl(url);
+  byteLength ||= await byteLengthFromUrl(url, { headers });
   return {
     byteLength,
     async slice(start, end) {
       // fetch byte range from url
-      const headers = new Headers(customHeaders);
       const endStr = end === undefined ? "" : end - 1;
-      headers.set("Range", `bytes=${start.toString()}-${endStr.toString()}`);
-      const res = await fetch(url, { headers });
+      const res = await fetch(url, {
+        headers: {
+          ...headers,
+          range: `bytes=${start.toString()}-${endStr.toString()}`,
+        },
+      });
       if (!res.ok || !res.body)
         throw new Error(`fetch failed ${res.status.toString()}`);
       return res.arrayBuffer();
