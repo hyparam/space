ad16c954ad56b881f6fcf97667570bec689c7784
Sylvain Lesage <severo@rednegra.net>
2024-12-03 14:52:38 +0100

    ignore coverage in eslint

--- eslint.config.js
+++ eslint.config.js
@@ -49,7 +49,7 @@ const sharedTsRules = {
 }
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ['dist', 'coverage'] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.strictTypeChecked, ...tseslint.configs.stylisticTypeChecked],
     // Set the react version
